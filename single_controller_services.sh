ad0c18e7c92cb26094a5e51682452d1c85312e8f
willb <willb@linuxacademy.com>
2018-08-23 16:36:03 -0400

    wait for cluster to come up before trying to configure kubelet rbac

--- single_controller_services.sh
+++ single_controller_services.sh
@@ -141,7 +141,7 @@ EOF
 systemctl daemon-reload
 systemctl enable kube-apiserver kube-controller-manager kube-scheduler
 systemctl start kube-apiserver kube-controller-manager kube-scheduler
-sleep 5
+until kubectl get componentstatuses --kubeconfig /home/cloud_user/admin.kubeconfig; do sleep 5; done
 cat << EOF | kubectl apply --kubeconfig /home/cloud_user/admin.kubeconfig -f -
 apiVersion: rbac.authorization.k8s.io/v1beta1
 kind: ClusterRole
